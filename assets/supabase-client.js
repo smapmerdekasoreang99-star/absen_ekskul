dd266cd7c835fdba9a83a7e4feea0becfa401cb9
smapmerdekasoreang99-star <smapmerdekasoreang99@gmail.com>
2026-09-14 10:05:27 +0700

    Update supabase-client.js

--- assets/supabase-client.js
+++ assets/supabase-client.js
@@ -18,9 +18,9 @@ export const SUPABASE_ANON_KEY = 'sb_publishable_rjHVGT0ULc03TC2ljIytSA_2X54xzR1
 
 // --- Project Tryout_guru (sumber data siswa) -------------------------
 export const SUMBER_SISWA = {
-  url:   'https://jzxcnfetpjkltjjbglxz.supabase.co',   // kosongkan '' bila satu project dengan ekskul
-  key:   'sb_publishable_9pl5IOJl-Vx0KEnHtCs3nA_ioZOkacq',
-  tabel: 'tka_siswa_ekskul', // tampilan terbatas dari db/akses_siswa.sql
+  url:   '',   // kosongkan '' bila satu project dengan ekskul
+  key:   '',
+  tabel: 'v_siswa_aktif', // tampilan terbatas dari db/akses_siswa.sql
                              // ganti 'tka_siswa' bila ingin langsung ke tabelnya
   id:    'nisn',
   nis:   'nis',
